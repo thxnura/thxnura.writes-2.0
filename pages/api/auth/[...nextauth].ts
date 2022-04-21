53d3a03fdf28b0c1876e2cfee9b49fe00b7f39d0
Thanura Manjitha Peiris <85317019+thxnura@users.noreply.github.com>
2022-04-21 16:23:44 +0530

    final4

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,7 +4,8 @@ import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GitHubProvider from 'next-auth/providers/github';
-import prisma from '../../../lib/prisma';
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
